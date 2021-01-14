e11cb060ded5733ae0e4846314ced6d151ecde8e
jeonsooyoung <peace2332@naver.com>
2021-01-14 23:55:10 +0900

    DetailContainer1

--- jenflix/src/Routes/Detail/DetailContainer.js
+++ jenflix/src/Routes/Detail/DetailContainer.js
@@ -8,8 +8,24 @@ export default class extends React.Component {
     loading: true
   };
 
+  async componentDidMount() {
+    const {
+      match :{
+        params :{id}
+      },
+      history: {push}
+    } = this.props
+    const parsedId = parseInt(id);
+    if(isNaN(parsedId)){
+      return push("/")// Number가 아니면 Home으로 이동
+    }
+  }
+
   render() {
+    console.log(this.props)
     const { result, error, loading } = this.state;
     return <DetailPresenter result={result} error={error} loading={loading} />;
   }
 }
+
+// 1. URL 처리 :  우리가 detail에 있는지 show에 있는지 알아내야함
