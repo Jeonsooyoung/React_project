40ca8efecd3ddbb8f41dc1c158c56cd0bfcb7d06
jeonsooyoung <peace2332@naver.com>
2021-01-15 23:57:02 +0900

    detailContainer

--- jenflix/src/Routes/Detail/DetailContainer.js
+++ jenflix/src/Routes/Detail/DetailContainer.js
@@ -1,12 +1,18 @@
+import { moviesApi, tvApi } from "api";
 import React from "react";
 import DetailPresenter from "./DetailPresenter";
 
 export default class extends React.Component {
-  state = {
-    result: null,
-    error: null,
-    loading: true
-  };
+  constructor (props){
+    super(props);
+    const {location : {pathname}} = props;    
+    this.state = {
+      result: null,
+      error: null,
+      loading: true,
+      isMovie : pathname.includes("/movie/")
+    };
+  }
 
   async componentDidMount() {
     const {
@@ -15,14 +21,30 @@ export default class extends React.Component {
       },
       history: {push}
     } = this.props
+    const {isMovie} = this.state
     const parsedId = parseInt(id);
     if(isNaN(parsedId)){
       return push("/")// Number가 아니면 Home으로 이동
     }
+    let result = null;
+    if(isMovie){
+      try{
+        if(isMovie){
+          result = await moviesApi.movieDetail(parsedId)
+        }else {
+          result = await tvApi.showDetail(parsedId)
+        }
+        console.log(result)
+      } catch {
+        this.setState({error:"Can't find anything."})
+      } finally{
+        this.setState({loading:false, result})
+      }
+    }
+
   }
 
   render() {
-    console.log(this.props)
     const { result, error, loading } = this.state;
     return <DetailPresenter result={result} error={error} loading={loading} />;
   }
